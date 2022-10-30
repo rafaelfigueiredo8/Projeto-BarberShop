9a5758705b265de4002cb33ab3b1e581b6d73332
Rafael Figueiredo <rafaelferreira-10@hotmail.com>
2022-10-29 21:24:20 -0300

    Criação das classes Repository

---
+++ src/main/java/com/cotemig/barbershop/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package com.cotemig.barbershop.repository;
+
+import com.cotemig.barbershop.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+}
